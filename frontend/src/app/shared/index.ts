a7cf5d0ad5f87c416f3e52da70de871723a5e819
hrvanovicm <hrvanovicmirza64@gmail.com>
2025-09-11 23:41:22 +0200

    setup shared utilities and configurations: add default Angular Material dialog configurations, `.editorconfig` for consistent editor settings, and `.gitignore` rules for clean project structure

---
+++ frontend/src/app/shared/index.ts
@@ -0,0 +1,8 @@
+import { MatDialogConfig } from '@angular/material/dialog';
+
+export const defaultDialogConfig: Partial<MatDialogConfig> = {
+  width: '600px',
+  height: '80vh',
+  maxHeight: '1200px',
+  maxWidth: '600px',
+};
